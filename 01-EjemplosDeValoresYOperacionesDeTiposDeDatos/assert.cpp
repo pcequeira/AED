78f61cccb0062542afe275a1d63095c422e7fe63
pcequeira <83880811+pcequeira@users.noreply.github.com>
2022-05-06 00:14:46 -0300

    Update assert.cpp

--- 01-EjemplosDeValoresYOperacionesDeTiposDeDatos/assert.cpp
+++ 01-EjemplosDeValoresYOperacionesDeTiposDeDatos/assert.cpp
@@ -17,5 +17,5 @@ int main() {
     assert (2.0==1.0+1.0);
     assert (1.0==0.1*10.0);
     assert (1.0!=0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1);
-    std::cout << std::setprecision(17)
+    std::cout << std::setprecision(17);
   }
