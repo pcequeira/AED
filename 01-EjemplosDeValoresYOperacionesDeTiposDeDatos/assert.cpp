24e4389161ea9a7c03f33682137e3b349a08abcb
pcequeira <83880811+pcequeira@users.noreply.github.com>
2022-05-06 00:13:21 -0300

    Create assert.cpp

---
+++ 01-EjemplosDeValoresYOperacionesDeTiposDeDatos/assert.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+
+int main() {
+    assert (2 == 1 + 1);
+    // bool
+    assert (true);
+    assert (false==false);
+    assert (false!=true);
+    assert (not false);
+    assert (false == not true);
+    assert (true and true);
+    assert (false or true);
+    assert ((false and false) == false);
+    // double
+    assert (2.0==1.0+1.0);
+    assert (1.0==0.1*10.0);
+    assert (1.0!=0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1);
+    std::cout << std::setprecision(17)
+  }
